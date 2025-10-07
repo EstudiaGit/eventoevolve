831dfeb53a66400fcdba788bf9a581cbedede802
EStudiaGit <estudia.git@gmail.com>
2025-10-07 12:44:18 +0100

    routes fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-// astro.config.mjs
+/*// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
@@ -42,6 +42,36 @@ export default defineConfig({
       // Hot Module Replacement
       hmr: {
         overlay: true, // Mostrar errores en pantalla
+      },
+    },
+  },
+});*/
+
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://EstudiaGit.github.io",
+  //base: "/eventotickets",
+  output: "static",
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+  ],
+  server: {
+    host: "0.0.0.0",
+    port: 4321,
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      },
+      hmr: {
+        overlay: true,
       },
     },
   },
